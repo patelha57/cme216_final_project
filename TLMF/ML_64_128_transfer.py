aa240a9bdb9a4021032713650b0610104d2a248c
patelha57 <patelha57@gmail.com>
2023-03-09 11:26:04 -0800

    Clean-up TLMF driver script

--- TLMF/ML_64_128_transfer.py
+++ TLMF/ML_64_128_transfer.py
@@ -1,19 +1,6 @@
+import time
 import numpy as np
 import torch
-import torch.nn as nn
-import torch.optim as optim
-import torch.utils.data as utils
-from torch.utils.data import DataLoader
-from torch.utils.data import sampler
-from torch.optim.lr_scheduler import ReduceLROnPlateau, StepLR, MultiStepLR
-from torch.autograd import Variable
-from h5py import File
-import h5py
-import torchvision.datasets as dset
-import torchvision.transforms as T
-import torch.nn.functional as F
-import copy
-import time
 
 from dense_ed_tlmf import *
 from utils_tlmf import *
@@ -58,24 +45,24 @@ n_test = 100  # number of test data
 model_file_name = "final_model.pth"
 
 # PHASE1 Experimental controls
-reps_phase1 = 2        # repetitions for phase1
-n_epochs_phase1 = 50   # epochs in phase1
+reps_phase1 = 2          # repetitions for phase1
+n_epochs_phase1 = 50     # epochs in phase1
 lr_phase1 = 0.0005       # learning rate in phase1
 wd_phase1 = 1e-5         # weight decay in phase1
 factor_phase1 = 0.6      # factor for "ReduceLROnPlateau" in phase1
 min_lr_phase1 = 1.5e-06  # minimum learning rate in phase1
 
 # PHASE2 Experimental controls
-reps_phase2 = 2        # repetitions for phase2
-n_epochs_phase2 = 50   # epochs in phase2
+reps_phase2 = 2          # repetitions for phase2
+n_epochs_phase2 = 50     # epochs in phase2
 lr_phase2 = 0.00005      # learning rate in phase2
 wd_phase2 = 1e-5         # weight decay in phase2
 factor_phase2 = 0.6      # factor for "ReduceLROnPlateau" in phase2
 min_lr_phase2 = 1.5e-06  # minimum learning rate in phase2
 
 # PHASE3 Experimental controls
-reps_phase3 = 2        # repetitions for phase3
-n_epochs_phase3 = 50   # epochs in phase3
+reps_phase3 = 2          # repetitions for phase3
+n_epochs_phase3 = 50     # epochs in phase3
 lr_phase3 = 0.00001      # learning rate in phase3
 wd_phase3 = 1e-5         # weight decay in phase3
 factor_phase3 = 0.6      # factor for "ReduceLROnPlateau" in phase3
@@ -89,7 +76,7 @@ Device Selection (CUDA)
 #################################################################
 '''
 USE_GPU = True
-dtype = torch.float32 # we will be using float throughout this tutorial
+dtype = torch.float32  # we will be using float throughout this tutorial
 if USE_GPU and torch.cuda.is_available():
     device = torch.device('cuda')
 else:
@@ -106,34 +93,28 @@ Reads the data in the .hdf5 files and adds it to "dat" variable
 #################################################################
 '''
 
+data_dir = "/home/yrshen/Desktop/TLMF/"
 
-
-
-
-#########################Parse Euler data 
-eulerDataRootPath = "/home/yrshen/Desktop/TLMF/euler_data/"
+# parse Euler data
+eulerDataRootPath = f"{data_dir}/euler_data"
 dataFileName = "surface_flow.dat"
 eulerData = parseData.parse_data(eulerDataRootPath, dataFileName)
 eulerData = np.asarray(eulerData)
 
-#########################Parse RANS data
-ransDataRootPath =  "/home/yrshen/Desktop/TLMF/rans_data/"
+# parse RANS data
+ransDataRootPath = f"{data_dir}/rans_data"
 ransData = parseData.parse_data(ransDataRootPath, dataFileName)
 ransData = np.asarray(ransData)
 
-
 dat = {}
 
-dat['ks_lfs'] = eulerData[:,:,2]
-dat['ps_lfs'] = eulerData[:,:,3]
-dat['ss_lfs'] = eulerData[:,:,4]
+dat['ks_lfs'] = eulerData[:, :, 2]
+dat['ps_lfs'] = eulerData[:, :, 3]
+dat['ss_lfs'] = eulerData[:, :, 4]
 
-
-dat['ks_hfs'] = ransData[:,:,2]
-dat['ps_hfs'] = ransData[:,:,3]
-dat['ss_hfs'] = ransData[:,:,4]
-
-
+dat['ks_hfs'] = ransData[:, :, 2]
+dat['ps_hfs'] = ransData[:, :, 3]
+dat['ss_hfs'] = ransData[:, :, 4]
 
 '''
 #################################################################
@@ -147,20 +128,17 @@ Phase1 - experiment [LFS]
 #################################################################
 '''
 # Build LFS data loaders
-test_loader_lfs, dat = \
-    loader_test(data=dat,num_test=n_test,Nxy=(64,64),bs=40,scale='lfs')
+test_loader_lfs, dat = loader_test(data=dat, num_test=n_test, Nxy=(64, 64), bs=40, scale='lfs')
 
-train_loader = loader_train(data=dat, scale='lfs', \
-                            num_training=n_lfs, Nxy=(64,64), bs=40, order=0)
+train_loader = loader_train(data=dat, scale='lfs', num_training=n_lfs, Nxy=(64, 64), bs=40, order=0)
 
 #########################################################
 #########################################################
 # Build model
 model_orig = None
-model_orig = DenseED(1, 16, blocks=(7,12,7), growth_rate=40,
-                        drop_rate=0, bn_size=8,
-                        num_init_features=64, bottleneck=False).to(device)
-model_phase1_orig = DenseED_phase1(model_orig,blocks=(7,12,7)).to(device)
+model_orig = DenseED(1, 16, blocks=(7, 12, 7), growth_rate=40, drop_rate=0, bn_size=8, num_init_features=64,
+                     bottleneck=False).to(device)
+model_phase1_orig = DenseED_phase1(model_orig, blocks=(7, 12, 7)).to(device)
 
 ##########################################################
 ##########################################################
@@ -183,15 +161,14 @@ Phase2 - experiment [HFS1]
 #################################################################
 '''
 # Build HFS data loaders
-test_loader_hfs, dat = \
-    loader_test(data=dat,num_test=n_test,Nxy=(128,128),bs=40,scale='hfs')
+test_loader_hfs, dat = loader_test(data=dat, num_test=n_test, Nxy=(128, 128), bs=40, scale='hfs')
 
-train_loader = loader_train(data=dat, scale='hfs', \
-                                      num_training=n_hfs, Nxy=(128,128), bs=40, order=0)
+train_loader = loader_train(data=dat, scale='hfs', num_training=n_hfs, Nxy=(128, 128), bs=40, order=0)
+
 #########################################################
 #########################################################
 # Build model
-model_phase2_orig = DenseED_phase2(model_orig,model_phase1)
+model_phase2_orig = DenseED_phase2(model_orig, model_phase1)
 
 # Freeze weights from being updated (whole model)
 for param in model_phase2_orig.parameters():
@@ -227,6 +204,7 @@ model_phase3_orig = model_phase2
 # Unfreeze all weights
 for param in model_phase3_orig.parameters():
     param.requires_grad = True
+
 #########################################################
 #########################################################
 # Train
@@ -240,5 +218,4 @@ print(f"PHASE3 RMSE BEST = {rmse_best}")
 torch.save(model_phase3.state_dict(), model_file_name)
 
 toc_all = time.time()
-print('EXPERIMENT COMPLETE, TIME ELAPSED: {} [sec]'
-      .format(toc_all-tic_all))
+print(f'EXPERIMENT COMPLETE, TIME ELAPSED: {toc_all - tic_all} [sec]')
